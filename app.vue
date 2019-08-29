16f67b9fbea3b822d1633dd77a10e64982ca04d5
Zegendary <zxw1992513@qq.com>
2019-08-29 10:16:22 +0800

    init

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    1111
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bundler: 'Parcel'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('hello java')
+import Vue from 'vue'
+import App from './app.vue'
+
+new Vue(App).$mount('#app')
