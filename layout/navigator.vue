3c80e72e2d6926a8d71677f46038cb0d5df58dea
Big7lion <biglion77@outlook.com>
2019-09-06 11:12:23 +0800

    修改

--- layout/navigator.vue
+++ layout/navigator.vue
@@ -16,10 +16,10 @@
 				</a>
 				<div class="items">
 					<a href="#">介绍</a>
-					<a href="../detail.html">课程详情</a>
-					<a href="../projects.html">教学模式</a>
-					<a href="../mode.html">真实的故事</a>
-					<a href="../about.html">关于我们</a>
+					<a href="detail">课程详情</a>
+					<a href="projects">教学模式</a>
+					<a href="mode">真实的故事</a>
+					<a href="about">关于我们</a>
 				</div>
 			</div>
 		</nav>
