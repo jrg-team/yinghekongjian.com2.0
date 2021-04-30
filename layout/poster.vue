bf029d166bbba8e2db35eb14f1bfc43d799bd504
BarryZhu <zhuye256@163.com>
2021-04-30 15:10:23 +0800

    调整界面，不再展示视频，而是展示一个大图

---
+++ layout/poster.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="poster-wrapper">
+        <a :href="descriptionUrl" target="_blank">
+            <img class="poster" :src="src" alt="封面" />
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "poster",
+        data() {
+            return {
+                src: 'https://static.xiedaimala.com/xdml/image/6e556a51-b8ff-466f-bda6-7d1847e39f2e/2019-10-15-16-2-45.png',
+                descriptionUrl: "https://mp.weixin.qq.com/s/sWuDYKcYhJtPcOhxPJVQcw"
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .poster-wrapper {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+    .poster {
+        max-width: 80vw;
+    }
+    @media (min-width: 500px) {
+        max-width: 90vw;
+    }
+</style>
